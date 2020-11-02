6f15be4a2548f631b24b426444928409878e2664
GustavoCarreon24 <gustavocarreon24@yahoo.com.mx>
2020-11-01 23:55:01 -0600

    Actualizacion 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/30/70f559b3ce1c001b1d64ede09399499a
@@ -0,0 +1,18 @@
+package PackdeHilos;
+
+public class Main 
+{
+	
+	public static void main (String[] args)
+	{
+		
+		Thread hilo1 = new Proceso("Permiso Denegado!");
+		Thread hilo2 = new Proceso("Permiso Concedido!");
+		
+		hilo1.start();
+		hilo2.start();
+		
+	}
+	
+	
+}
--- ProgramaHilos/src/PackdeHilos/Main.java
+++ ProgramaHilos/src/PackdeHilos/Main.java
@@ -6,11 +6,14 @@ public class Main
 	public static void main (String[] args)
 	{
 		
-		Thread hilo1 = new Proceso("Permiso Denegado!");
-		Thread hilo2 = new Proceso("Permiso Concedido!");
+		Thread hilo1 = new Proceso("Identifiquese!");
+		Thread hilo2 = new Proceso("...Reconociendo...");
+		Thread hilo3 = new Proceso("Acceso Concedido!");
 		
 		hilo1.start();
 		hilo2.start();
+		hilo3.start();
+		
 		
 	}
 	
